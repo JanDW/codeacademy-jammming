63efee4f6f5508a76e5a5fae44a9ce4a4da9f148
Jan De Wilde <jandw@mit.edu>
2019-04-04 13:45:53 -0400

    Updates

---
+++ precache-manifest.918c62867b256fd676f263ec6c655011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/codeacademy-jamming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "5ec51a18ffc5298a249d",
+    "url": "/codeacademy-jamming/static/js/runtime~main.6da3a9b8.js"
+  },
+  {
+    "revision": "1efd3b9b55eb4e7ba07a",
+    "url": "/codeacademy-jamming/static/js/main.549f6b94.chunk.js"
+  },
+  {
+    "revision": "a3ff84d27d844bb31518",
+    "url": "/codeacademy-jamming/static/js/2.f3f7640a.chunk.js"
+  },
+  {
+    "revision": "1efd3b9b55eb4e7ba07a",
+    "url": "/codeacademy-jamming/static/css/main.0d3299ee.chunk.css"
+  },
+  {
+    "revision": "c314c696482b0f9492264c36b2d84ff4",
+    "url": "/codeacademy-jamming/index.html"
+  }
+];
--- src/util/util.js
+++
@@ -1,6 +0,0 @@
-export const createQueryString = paramsObj =>
-  Object.keys(paramsObj)
-    .map(key => {
-      return encodeURIComponent(key) + '=' + encodeURIComponent(paramsObj[key]);
-    })
-    .join('&');
